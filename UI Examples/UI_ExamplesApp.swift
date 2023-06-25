8790950b0990aeaafc01a65149464addaf421eef
Nicholas Pompea <nick.pompea@gmail,com>
2023-06-24 21:12:24 -0600

    Initial Commit

---
+++ UI Examples/UI_ExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  UI_ExamplesApp.swift
+//  UI Examples
+//
+//  Created by Nicholas Pompea on 6/24/23.
+//
+
+import SwiftUI
+
+@main
+struct UI_ExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
